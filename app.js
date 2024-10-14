c600f7d2ea5b31a09ac38c57579361278a66e6a1
Hamza0smit <hussainmubasher52@gmail.com>
2024-10-14 00:22:17 -0700

    no commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var userValue =prompt("Enter the number")
+
+ for (let i = 1; i <=userValue; i++){
+    for(let j = 1;j <= i; j++){
+        document.write(j + " ");
+    }
+    document.write("<br>");
+ }
+
+
+ var userValue =prompt("Enter the second number")
+
+ for (let i = 1; i <=userValue; i++){
+    for(let j = 1;j <= i; j++){
+        document.write(  " * ");
+    }
+    document.write("<br>");
+ }
